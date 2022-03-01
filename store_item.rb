9601db27a1f8a7211c2d717647c89a773b200f83
jeffspatafora <jeffspatafora@gmail.com>
2022-03-01 11:36:51 -0800

    created store_item list

---
+++ store_item.rb
@@ -0,0 +1,9 @@
+guitar1 = {brand: "fender", color: "green", price: 1200}
+guitar2 = {brand: "gibson", color: "black", price: 1500}
+guitar3 = {:brand => "ibanez", :color => "purple", :price => 500}
+
+p "Guitar 1 is a #{guitar1[:color]} #{guitar1[:brand]} that costs #{guitar1[:price]}."
+
+p "Guitar 2 is a #{guitar2[:color]} #{guitar2[:brand]} that costs #{guitar2[:price]}."
+
+p "Guitar 2 is a #{guitar3[:color]} #{guitar3[:brand]} that costs #{guitar3[:price]}."
